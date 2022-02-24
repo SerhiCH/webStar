7e562d5308280c4850485bb390bedfe6452fadc6
SerhiiCH <98918888+SerhiCH@users.noreply.github.com>
2022-02-24 07:27:23 +0200

    Add files via upload

---
+++ js/extension.js
@@ -0,0 +1,11 @@
+VanillaTilt.init(document.querySelector(".glassmorphic-card"), {
+    max: 15,
+    speed: 200,
+    glare: true,
+    "max-glare": 1,
+});
+
+
+$('button').on('click', function(){     
+    window.location.href = $(this).attr('url');
+});
